346bbe12ce8299943610b8c3e484816f372adefe
Anton <anton.shevyrin@gmail.com>
2017-08-29 22:12:58 +0300

    Quote changed to stateless pure view component. Quotes loading went to App. Added "New quote" button (without styles yet).

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import Quote from './Quote';
 
-/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable spaced-comment, no-console */
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			quoteText: '',
+			quoteAuthor: '',
+			quotesAreLoaded: false,
+			availableQuotes: [],
+		};
+
+		this.setRandomQuote = this.setRandomQuote.bind(this);
+	}
+
+	componentDidMount() {
+		this.request = axios.get('./quotes.json').then((result) => {
+			this.setState({
+				quotesAreLoaded: true,
+				availableQuotes: result.data.quotes,
+			});
+			this.setRandomQuote();
+		});
+	}
+
+	componentWillUnmount() {
+		this.request.abort();
+	}
+
+	setRandomQuote() {
+		const randomIndex = Math.floor(this.state.availableQuotes.length * Math.random());
+		this.setState({
+			quoteText: this.state.availableQuotes[randomIndex].quoteText,
+			quoteAuthor: this.state.availableQuotes[randomIndex].quoteAuthor,
+		});
+	}
+
 	render() {
 		return (<div className="App">
-			<Quote />
+			<Quote quoteText={this.state.quoteText} quoteAuthor={this.state.quoteAuthor} />
+			<button onClick={this.setRandomQuote}>New quote</button>
 		</div>);
 	}
 }
--- src/Quote.js
+++ src/Quote.js
@@ -1,55 +1,23 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 import FontAwesome from 'react-fontawesome';
 import './Quote.css';
 
-class Quote extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			quoteText: '',
-			quoteAuthor: '',
-			quotesAreLoaded: false,
-			availableQuotes: [],
-		};
-	}
+const Quote = ({ quoteText, quoteAuthor }) => (
+	<div className="Quote">
+		<blockquote className="blockquote mb-0">
+			<p>
+				<FontAwesome className="fa-quote-left" name="leftQuote" />&nbsp;
+				{quoteText}&nbsp;
+				<FontAwesome className="fa-quote-right" name="rightQuote" />
+			</p>
+			<footer className="blockquote-footer">{quoteAuthor}</footer>
+		</blockquote>
+	</div>
+);
 
-	componentDidMount() {
-		this.request = axios.get('./quotes.json').then((result) => {
-			this.setState({
-				quotesAreLoaded: true,
-				availableQuotes: result.data.quotes,
-			});
-			this.setRandomQuote();
-		});
-	}
-
-	componentWillUnmount() {
-		this.request.abort();
-	}
-
-	setRandomQuote() {
-		const randomIndex = Math.floor(this.state.availableQuotes.length * Math.random());
-		this.setState({
-			quoteText: this.state.availableQuotes[randomIndex].quoteText,
-			quoteAuthor: this.state.availableQuotes[randomIndex].quoteAuthor,
-		});
-	}
-
-	render() {
-		return (
-			<div className="Quote">
-				<blockquote className="blockquote mb-0">
-					<p>
-						<FontAwesome className="fa-quote-left" name="leftQuote" />&nbsp;
-						{this.state.quoteText}&nbsp;
-						<FontAwesome className="fa-quote-right" name="rightQuote" />
-					</p>
-					<footer className="blockquote-footer">{this.state.quoteAuthor}</footer>
-				</blockquote>
-			</div>
-		);
-	}
-}
+Quote.propTypes = {
+	quoteText: React.PropTypes.string.isRequired,
+	quoteAuthor: React.PropTypes.string.isRequired,
+};
 
 export default Quote;
