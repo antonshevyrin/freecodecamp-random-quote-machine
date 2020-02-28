6d038b931308f7359a717c646119051393d0db22
Anton Shevyrin <anton.shevyrin@gmail.com>
2020-02-29 00:12:51 +0200

    Updates

---
+++ precache-manifest.2d6037daebeb21d1972adc80420c5ee6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64d43bb4ac9990d1cf0ff371beb4c1a",
+    "url": "/freecodecamp-random-quote-machine/index.html"
+  },
+  {
+    "revision": "7a3947f2000268eeb055",
+    "url": "/freecodecamp-random-quote-machine/static/css/2.bb728fb0.chunk.css"
+  },
+  {
+    "revision": "a24fcbbbc6c95e3675cc",
+    "url": "/freecodecamp-random-quote-machine/static/css/main.731ff696.chunk.css"
+  },
+  {
+    "revision": "7a3947f2000268eeb055",
+    "url": "/freecodecamp-random-quote-machine/static/js/2.6d4634db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/freecodecamp-random-quote-machine/static/js/2.6d4634db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24fcbbbc6c95e3675cc",
+    "url": "/freecodecamp-random-quote-machine/static/js/main.4fbcf2a2.chunk.js"
+  },
+  {
+    "revision": "f56ef862b9cc4a35340d",
+    "url": "/freecodecamp-random-quote-machine/static/js/runtime-main.55e1d981.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/freecodecamp-random-quote-machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/freecodecamp-random-quote-machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/freecodecamp-random-quote-machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/freecodecamp-random-quote-machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/freecodecamp-random-quote-machine/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/freecodecamp-random-quote-machine/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/freecodecamp-random-quote-machine/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/freecodecamp-random-quote-machine/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/freecodecamp-random-quote-machine/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/freecodecamp-random-quote-machine/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1e252e7ab370f63ac3e07cc1d8b1877e.js"
+  "/freecodecamp-random-quote-machine/precache-manifest.2d6037daebeb21d1972adc80420c5ee6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/freecodecamp-random-quote-machine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
